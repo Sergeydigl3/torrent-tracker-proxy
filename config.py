bec9d5edbdda4b301cbb67c9cebb2aaad0a2ec4c
Sergeydigl3 <26508358+Sergeydigl3@users.noreply.github.com>
2024-06-20 16:38:31 +0300

    Added fixed port to ENV

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ dotenv.load_dotenv(".env")
 announce_url_const = os.environ.get("ANNOUNCE_URL")
 peer_id_const = os.environ.get("PEER_ID")
 user_agent_tracker = os.environ.get("USER_AGENT_TRACKER")
+fake_port = os.environ.get("FAKE_PORT")
 
 use_proxy_trackers = bool(int(os.environ.get("USE_PROXY_TRACKERS", "0")))
 http_proxy_trackers = os.environ.get("HTTP_PROXY_TRACKERS")
--- test_client.py
+++ test_client.py
@@ -1,6 +1,6 @@
 import time
 
-from config import announce_url_const, peer_id_const
+from config import announce_url_const, peer_id_const, fake_port
 from models import TrackerClient
 
 info_hash = "26ee74d6aafcc563458c56457a38844cfe5b95f1"
@@ -9,6 +9,7 @@ trackerClient = TrackerClient(
     info_hash,
     peer_id_const,
     left=818119347,
+    port=fake_port
 )
 
 trackerClient.start_background_update()
